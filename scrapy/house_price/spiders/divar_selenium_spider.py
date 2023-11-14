89e3585215f6df306b7d2d8db7506c54765fd15a
Fatemeh <fatemeh_narjili@yahoo.com>
2023-11-14 10:54:53 +0330

    Update divar_selenium spider

--- scrapy/house_price/spiders/divar_selenium_spider.py
+++ scrapy/house_price/spiders/divar_selenium_spider.py
@@ -10,8 +10,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from time import sleep
-# import splash
-# from scrapy_splash import SplashRequest
+import json
 
 
 class DivarSeleniumSpider(scrapy.Spider):
@@ -37,38 +36,47 @@ class DivarSeleniumSpider(scrapy.Spider):
             end_of_scroll = my_scroll
             elems = driver.find_elements(
                 By.CSS_SELECTOR, "div.post-card-item-af972.kt-col-6-bee95.kt-col-xxl-4-e9d46 a[href]")
-            detail_page_links = [elem.get_attribute('href') for elem in elems]
+            list_of_tokens = [elem.get_attribute(
+                'href').split("/")[-1] for elem in elems]
 
-            for link in detail_page_links:
-                driver_detail = webdriver.Chrome()
-                driver_detail.get(link)
-                sleep(2)
-                # elems = driver.find_elements(By.XPATH,'/html/body/div[1]/div[2]/div/nav/ol/li[3]/a/span')
-                elems = driver.find_elements(By.XPATH, '//html//body//')
+            detail_page_links = []
+            for token in list_of_tokens:
+                detail_page_links.append(
+                    f"https://api.divar.ir/v8/posts/{token}")
+            yield from response.follow_all(detail_page_links, self.parse_detail)
 
-                break
-                # elems = driver.find_elements(By.CSS_SELECTOR','')
+            sleep(2)
 
-                print(
-                    "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-                print(elems)
-                # element = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR,'div.kt-page-title__title.kt-page-title__title--responsive-sized')))
-                # links = [elem.get_attribute("innerHTML") for elem in elems]
-                # links=element.text
-                # print("---------------------------------------------------")
-                # print(links)
-                yield {"a": elems}
+            # for link in detail_page_links:
+            #     driver_detail = webdriver.Chrome()
+            #     driver_detail.get(link)
+            #     sleep(2)
+            #     # elems = driver.find_elements(By.XPATH,'/html/body/div[1]/div[2]/div/nav/ol/li[3]/a/span')
+            #     elems = driver.find_elements(By.XPATH, '//html//body//')
 
-                # print("---------------------------------------------------------------------------")
-                # for element in elems:
-                #     print(element.text)
-                #     yield element.text
+            #     break
+            #     # elems = driver.find_elements(By.CSS_SELECTOR','')
 
-                # yield elems
-                # with open('dd.txt', 'a+') as f:
-                #     for text in texts:
-                #         f.write(text.text+"\n")
-                # f.close()
+            #     print(
+            #         "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+            #     print(elems)
+            #     # element = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR,'div.kt-page-title__title.kt-page-title__title--responsive-sized')))
+            #     # links = [elem.get_attribute("innerHTML") for elem in elems]
+            #     # links=element.text
+            #     # print("---------------------------------------------------")
+            #     # print(links)
+            #     yield {"a": elems}
+
+            # print("---------------------------------------------------------------------------")
+            # for element in elems:
+            #     print(element.text)
+            #     yield element.text
+
+            # yield elems
+            # with open('dd.txt', 'a+') as f:
+            #     for text in texts:
+            #         f.write(text.text+"\n")
+            # f.close()
         #     for url in detail_page_links:
         #         yield SplashRequest(url, self.parse_detail, meta={
         #         'splash': {
@@ -77,9 +85,95 @@ class DivarSeleniumSpider(scrapy.Spider):
         #         }
         # })
             # yield from response.follow_all(detail_page_links, self.parse_detail)
-            sleep(2)
 
-    # def parse_detail(self, response):
-    #     print("---------------------------------------------------------------------------")
-    #     # price = response.css('script::text').getall()[-1]
-    #     yield {"price": response.body}
+    def parse_detail(self, response):
+        district_name = None
+        real_estate_area = None
+        real_estate_age = None
+        total_rooms = None
+        price = None
+        price_per_meter = None
+        real_estate_agencey = None
+        floor = None
+        elevator = None
+        parking = None
+        warehouse = None
+        units_per_floor = None
+        document = None
+        building_direction = None
+        unit_status = None
+        facilities = []
+
+        detail_data = json.loads(response.body.decode('utf-8'))
+
+        district_name = detail_data['data']['district']
+
+        for data in detail_data['widgets']['list_data']:
+            if data['title'] == "اطلاعات":
+                items = data.get("items")
+                for item in items:
+                    if item['title'] == "متراژ":
+                        real_estate_area = item['value']
+                    if item['title'] == "ساخت":
+                        real_estate_age = item['value']
+                    if item['title'] == "اتاق":
+                        total_rooms = item['value']
+
+            if data['title'] == "قیمت کل":
+                price = data['value']
+
+            if data['title'] == "قیمت هر متر":
+                price_per_meter = data['value']
+
+            if data['title'] == "آژانس املاک":
+                real_estate_agencey = data['value']
+
+            if data['title'] == "طبقه":
+                floor = data['value']
+
+            if data['title'] == "ویژگی‌ها و امکانات":
+                items = data.get("items")
+                for item in items:
+                    if item['title'] == "آسانسور":
+                        elevator = item['available']
+                    if item['title'] == "پارکینگ":
+                        parking = item['available']
+                    if item['title'] == "انباری":
+                        warehouse = item['available']
+
+                facility_page = data.get("next_page")
+                if facility_page is not None:
+                    facility_list = facility_page.get("widget_list")
+                    for facility in facility_list:
+                        if (facility['data'].get('text') == "ویژگی‌ها") or (facility['data'].get('text') == "امکانات"):
+                            pass
+                        elif facility['data']['title'] == "تعداد واحد در طبقه":
+                            units_per_floor = facility['data']['value']
+                        elif facility['data']['title'] == "سند":
+                            document = facility['data']['value']
+                        elif facility['data']['title'] == "جهت ساختمان":
+                            building_direction = facility['data']['value']
+                        elif facility['data']['title'] == "وضعیت واحد":
+                            unit_status = facility['data']['value']
+                        else:
+                            if facility['data']['has_divider'] == True:
+                                facilities.append(facility['data']['title'])
+
+        yield {
+            "district_name": district_name,
+            "real_estate_area": real_estate_area,
+            "real_estate_age": real_estate_age,
+            "total_rooms": total_rooms,
+            "price": price,
+            "price_per_meter": price_per_meter,
+            "real_estate_agencey": real_estate_agencey,
+            "floor": floor,
+            "elevator": elevator,
+            "parking": parking,
+            "warehouse": warehouse,
+            "units_per_floor": units_per_floor,
+            "document": document,
+            "building_direction": building_direction,
+            "unit_status": unit_status,
+            "facilities": facilities
+        }
