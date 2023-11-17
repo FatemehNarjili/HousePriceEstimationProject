99d6cc27c156958e7e7fc9e4169b0f0a0d80e40c
Fatemeh <fatemeh_narjili@yahoo.com>
2023-11-17 15:07:31 +0330

    Update divar_selenium spider

--- scrapy/house_price/spiders/divar_selenium_spider.py
+++ scrapy/house_price/spiders/divar_selenium_spider.py
@@ -1,5 +1,10 @@
+import json
+from time import sleep
+
 import scrapy
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
@@ -9,8 +14,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
-from time import sleep
-import json
 
 
 class DivarSeleniumSpider(scrapy.Spider):
@@ -19,13 +22,15 @@ class DivarSeleniumSpider(scrapy.Spider):
     start_urls = ["https://divar.ir/s/tehran/buy-residential"]
 
     def parse(self, response):
-        driver = webdriver.Chrome()
+        driver = webdriver.Chrome(service=ChromeService(
+            ChromeDriverManager().install()))
         driver.get('https://divar.ir/s/tehran/buy-residential')
         driver.implicitly_wait(20)
 
         end_of_scroll = driver.execute_script(
             "return document.body.scrollHeight")
-        for i in range(2):
+        i = 0
+        while True:
             driver.execute_script(
                 "window.scrollTo(0, document.body.scrollHeight)")
             sleep(2)
@@ -34,6 +39,8 @@ class DivarSeleniumSpider(scrapy.Spider):
             if my_scroll == end_of_scroll:
                 break
             end_of_scroll = my_scroll
+
+            sleep(5)
             elems = driver.find_elements(
                 By.CSS_SELECTOR, "div.post-card-item-af972.kt-col-6-bee95.kt-col-xxl-4-e9d46 a[href]")
             list_of_tokens = [elem.get_attribute(
@@ -45,46 +52,8 @@ class DivarSeleniumSpider(scrapy.Spider):
                     f"https://api.divar.ir/v8/posts/{token}")
             yield from response.follow_all(detail_page_links, self.parse_detail)
 
+            i += 1
             sleep(2)
-
-            # for link in detail_page_links:
-            #     driver_detail = webdriver.Chrome()
-            #     driver_detail.get(link)
-            #     sleep(2)
-            #     # elems = driver.find_elements(By.XPATH,'/html/body/div[1]/div[2]/div/nav/ol/li[3]/a/span')
-            #     elems = driver.find_elements(By.XPATH, '//html//body//')
-
-            #     break
-            #     # elems = driver.find_elements(By.CSS_SELECTOR','')
-
-            #     print(
-            #         "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-            #     print(elems)
-            #     # element = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR,'div.kt-page-title__title.kt-page-title__title--responsive-sized')))
-            #     # links = [elem.get_attribute("innerHTML") for elem in elems]
-            #     # links=element.text
-            #     # print("---------------------------------------------------")
-            #     # print(links)
-            #     yield {"a": elems}
-
-            # print("---------------------------------------------------------------------------")
-            # for element in elems:
-            #     print(element.text)
-            #     yield element.text
-
-            # yield elems
-            # with open('dd.txt', 'a+') as f:
-            #     for text in texts:
-            #         f.write(text.text+"\n")
-            # f.close()
-        #     for url in detail_page_links:
-        #         yield SplashRequest(url, self.parse_detail, meta={
-        #         'splash': {
-        #         'endpoint': 'render.html',
-        #         'args': {'wait': 0.5}
-        #         }
-        # })
-            # yield from response.follow_all(detail_page_links, self.parse_detail)
 
     def parse_detail(self, response):
         district_name = None
