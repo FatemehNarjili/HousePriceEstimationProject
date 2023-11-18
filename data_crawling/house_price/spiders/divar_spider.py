610db1e63385a2bec34605e0dfbbef5c6297f9d6
Fatemeh <fatemeh_narjili@yahoo.com>
2023-11-18 13:30:21 +0330

    Rename directories

---
+++ data_crawling/house_price/spiders/divar_spider.py
@@ -0,0 +1,127 @@
+import scrapy
+import json
+
+
+class DivarSpider(scrapy.Spider):
+    name = "divar"
+
+    start_urls = [
+        "https://api.divar.ir/v8/web-search/tehran/buy-residential?page=1"]
+
+    def __init__(self):
+        self.page_number = 1
+
+    def parse(self, response):
+
+        list_of_tokens = []
+        data = json.loads(response.body.decode('utf-8'))
+        post_list = data['web_widgets']['post_list']
+        for i in range(len(post_list)):
+            list_of_tokens.append(post_list[i]['data'].get('token', None))
+
+        detail_page_links = []
+        for token in list_of_tokens:
+            if token is not None:
+                detail_page_links.append(
+                    f"https://api.divar.ir/v8/posts/{token}")
+
+        yield from response.follow_all(detail_page_links, self.parse_detail)
+
+        self.page_number += 1
+        yield response.follow(f"https://api.divar.ir/v8/web-search/tehran/buy-residential?page={self.page_number}", self.parse)
+
+    def parse_detail(self, response):
+        district_name = None
+        real_estate_type = None
+        real_estate_area = None
+        real_estate_age = None
+        total_rooms = None
+        price = None
+        price_per_meter = None
+        real_estate_agencey = None
+        floor = None
+        elevator = None
+        parking = None
+        warehouse = None
+        units_per_floor = None
+        document = None
+        building_direction = None
+        unit_status = None
+        facilities = []
+
+        detail_data = json.loads(response.body.decode('utf-8'))
+
+        district_name = detail_data['data']['district']
+
+        real_estate_type = detail_data['data']['category']['slug']
+
+        for data in detail_data['widgets']['list_data']:
+            if data['title'] == "اطلاعات":
+                items = data.get("items")
+                for item in items:
+                    if item['title'] == "متراژ":
+                        real_estate_area = item['value']
+                    if item['title'] == "ساخت":
+                        real_estate_age = item['value']
+                    if item['title'] == "اتاق":
+                        total_rooms = item['value']
+
+            if data['title'] == "قیمت کل":
+                price = data['value']
+
+            if data['title'] == "قیمت هر متر":
+                price_per_meter = data['value']
+
+            if data['title'] == "آژانس املاک":
+                real_estate_agencey = data['value']
+
+            if data['title'] == "طبقه":
+                floor = data['value']
+
+            if data['title'] == "ویژگی‌ها و امکانات":
+                items = data.get("items")
+                for item in items:
+                    if item['title'] == "آسانسور":
+                        elevator = item['available']
+                    if item['title'] == "پارکینگ":
+                        parking = item['available']
+                    if item['title'] == "انباری":
+                        warehouse = item['available']
+
+                facility_page = data.get("next_page")
+                if facility_page is not None:
+                    facility_list = facility_page.get("widget_list")
+                    for facility in facility_list:
+                        if (facility['data'].get('text') == "ویژگی‌ها") or (facility['data'].get('text') == "امکانات"):
+                            pass
+                        elif facility['data']['title'] == "تعداد واحد در طبقه":
+                            units_per_floor = facility['data']['value']
+                        elif facility['data']['title'] == "سند":
+                            document = facility['data']['value']
+                        elif facility['data']['title'] == "جهت ساختمان":
+                            building_direction = facility['data']['value']
+                        elif facility['data']['title'] == "وضعیت واحد":
+                            unit_status = facility['data']['value']
+                        else:
+                            if facility['data']['has_divider'] == True:
+                                facilities.append(facility['data']['title'])
+
+        yield {
+            "district_name": district_name,
+            "real_estate_type": real_estate_type,
+            "real_estate_area": real_estate_area,
+            "real_estate_age": real_estate_age,
+            "total_rooms": total_rooms,
+            "price": price,
+            "price_per_meter": price_per_meter,
+            "real_estate_agencey": real_estate_agencey,
+            "floor": floor,
+            "elevator": elevator,
+            "parking": parking,
+            "warehouse": warehouse,
+            "units_per_floor": units_per_floor,
+            "document": document,
+            "building_direction": building_direction,
+            "unit_status": unit_status,
+            "facilities": facilities
+        }
---
+++ data_crawling/house_price/spiders/mrestate_spider.py
@@ -0,0 +1,76 @@
+import scrapy
+import re
+
+
+class MrestateSpider(scrapy.Spider):
+    name = "mrestate"
+
+    start_urls = [
+        "https://mrestate.ir/f/tehran/buy_residential_apartment",
+        "https://mrestate.ir/f/tehran/buy_residential_tower",
+        "https://mrestate.ir/f/tehran/buy_residential_penthouses",
+        "https://mrestate.ir/f/tehran/buy_residential_old-building",
+        "https://mrestate.ir/f/tehran/buy_residential_tenement",
+        "https://mrestate.ir/f/tehran/buy_residential_land",
+        "https://mrestate.ir/f/tehran/buy_residential_suite",
+        "https://mrestate.ir/f/tehran/buy_residential_villa"
+    ]
+
+    def parse(self, response):
+        detail_page_links = response.css("article.w-full.px-4.py-5 a")
+        yield from response.follow_all(detail_page_links, self.parse_detail)
+
+        next_page_link = response.css(
+            'a.flex.p-0.justify-center::attr(href)').getall()[-1]
+        yield response.follow(next_page_link, self.parse)
+
+    def parse_detail(self, response):
+        district_name = response.css(
+            'div.flex.items-center.text-14.font-medium.text-customBlack.mx-3::text').getall()[1]
+
+        real_estate_type = response.css(
+            'p.text-14.text-gray-400::text').getall()[2]
+
+        features = features = response.css(
+            'div.flex.flex-wrap.mb-6 div.text-sm.font-medium::text').getall()
+        features_length = len(features)
+
+        real_estate_area = int(re.findall(
+            r"\d+", features[0])[0]) if features_length > 0 else None
+        real_estate_age = (1402 - (int(re.findall(r"\d+", features[1])[0]))
+                           ) if features_length > 1 else None
+        total_rooms = int(re.findall(
+            r"\d+", features[2])[0]) if features_length > 2 else None
+        floor = int(re.findall(
+            r"\d+", features[2])[0]) if features_length > 3 else None
+
+        price = response.css(
+            'div.text-16.text-left.font-medium.text-customBlack::text').get()
+        price = "".join(price.split(" ")[0].split(","))
+
+        facilities = response.css(
+            'ul.flex.flex-wrap.mb-6.py-3 div.text-customBlack.px-3.text-sm.font-medium::text').getall()
+
+        for index, facility in enumerate(facilities):
+            if 'پارکینگ' in facility:
+                try:
+                    number_of_parking = int(re.findall(r"\d+", facility)[0])
+                except IndexError:
+                    number_of_parking = 0
+                facilities.pop(index)
+                break
+        else:
+            number_of_parking = 0
+
+        yield {
+            #     "district_number": district_number,
+            "district_name": district_name,
+            "real_estate_type": real_estate_type,
+            "real_estate_area": real_estate_area,
+            "total_rooms": total_rooms,
+            "number_of_parking": number_of_parking,
+            "real_estate_age": real_estate_age,
+            "price": price,
+            "facilities": facilities,
+            "floor": floor,
+        }
